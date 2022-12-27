cf5b671f0bcb4e0e77d8557e804a31801c2463fa
Jayesh Chaudhari <68538038+jayeshrocks34@users.noreply.github.com>
2022-12-27 16:40:46 +0530

    Update docker-script-ubuntu.sh

--- docker-script-ubuntu.sh
+++ docker-script-ubuntu.sh
@@ -7,4 +7,3 @@ sudo service docker start
 sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
